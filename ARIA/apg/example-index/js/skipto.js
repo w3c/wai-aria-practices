03c87612374bfa78c4ef4d5a43c42ae71dce9a82
Jon Gunderson <jongund@illinois.edu>
2022-06-16 12:39:25 -0500

    updating SKipTo default styling

--- ARIA/apg/example-index/js/skipto.js
+++ ARIA/apg/example-index/js/skipto.js
@@ -108,7 +108,7 @@
       'default': {
         fontFamily: 'Noto Sans, Trebuchet MS, Helvetica Neue, Arial, sans-serif',
         fontSize: `1.1rem`,
-        positionLeft: 0,
+        positionLeft: '0px',
         menuTextColor: '#000',
         menuBackgroundColor: '#def',
         menuitemFocusTextColor: '#fff',
