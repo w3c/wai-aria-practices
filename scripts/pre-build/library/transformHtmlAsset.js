2c1d77ad72f6a338254ab98a739dc79dcd040fc1
Howard Edwards <howarde.edwards@gmail.com>
2023-02-01 15:19:00 -0500

    Add frontmatter to .html assets

--- scripts/pre-build/library/transformHtmlAsset.js
+++ scripts/pre-build/library/transformHtmlAsset.js
@@ -1,7 +1,10 @@
 const { parse: parseHtml } = require("node-html-parser");
 const rewriteElementPaths = require("./rewriteElementPaths");
+const {rewriteSourcePath} = require("./rewritePath");
 
 const transformImageAsset = async (sourcePath, sourceContents) => {
+  const { sitePath } = rewriteSourcePath(sourcePath);
+
   if (sourcePath.endsWith("shared/templates/example-usage-warning.html")) {
     return sourceContents; // Will be transformed within each example page
   }
@@ -9,7 +12,11 @@ const transformImageAsset = async (sourcePath, sourceContents) => {
   const html = parseHtml(sourceContents);
   await rewriteElementPaths(html, { onSourcePath: sourcePath });
 
-  return html.outerHTML;
+  return `---
+layout: null
+permalink: ${sitePath}
+---
+${html.outerHTML}`;
 };
 
 module.exports = transformImageAsset;
