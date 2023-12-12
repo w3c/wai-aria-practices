742606bb8158fe582989b7b71247033316c8d543
alflennik <alflennik@users.noreply.github.com>
2023-12-12 10:02:48 -0800

    Remove fix because it should be a separate pr

--- scripts/pre-build/library/transformExample.js
+++ scripts/pre-build/library/transformExample.js
@@ -48,7 +48,13 @@ const transformExample = async (sourcePath, sourceContents) => {
 
   const getNotice = await loadedNotice;
   const notice = await getNotice(sourcePath);
-  html.querySelector("body").insertAdjacentHTML("afterbegin", notice);
+  html.querySelector("body").insertAdjacentHTML(
+    "afterbegin",
+    `
+      <h2 id="support-notice-header">Read This First</h2>
+      ${notice}
+    `
+  );
 
   const relatedLinksElement = html.querySelector(
     '[aria-label="Related Links"]'
