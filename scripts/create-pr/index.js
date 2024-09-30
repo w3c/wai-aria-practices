5f5ea8e3c7f56d14cbb556d227da35731614c8ec
Howard Edwards <howarde.edwards@gmail.com>
2024-09-30 10:46:52 -0400

    Update pr-create.yml to force a rebase to happen with main in the case generated branch doesn't have relevant changes; additional error handling coverage with create-pr script

--- scripts/create-pr/index.js
+++ scripts/create-pr/index.js
@@ -1,4 +1,4 @@
-const { Octokit } = require("@octokit/rest");
+const { Octokit } = require('@octokit/rest');
 
 // octokit should be authenticated with GITHUB_TOKEN from GA
 const octokit = new Octokit({
@@ -6,36 +6,54 @@ const octokit = new Octokit({
 });
 
 const jobId = process.env.JOB_ID;
-const isSuccess = process.env.OUTCOME === "success";
+// Outside of auth errors, the only other potential error is if the 'Update site files' step fails. Use this boolean.
+const isSiteFilesUpdateSuccess = process.env.OUTCOME === 'success';
 const repositoryOwner = process.env.REPO_OWNER;
-const previewLink = "aria-practices.netlify.app";
+const previewLink = 'aria-practices.netlify.app';
+const GENERATED_APG_WAI_BRANCH = 'apg/' + process.env.APG_BRANCH;
+
+const ERROR_APG_BODY_UPDATE = 101;
+const ERROR_SITE_FILES_UPDATE = 102;
+const ERROR_LIST_PULL_REQUESTS = 103;
+const ERROR_CREATE_PULL_REQUEST = 104;
+const ERROR_GET_PULL_REQUEST = 105;
+const ERROR_UPDATE_PULL_REQUEST = 106;
+// TODO: Uncomment when updated bot token is being used
+// const ERROR_CREATE_COMMIT_STATUS = 107;
 
 const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
   // Update APG PR
-  const getApgPrResult = await octokit.rest.pulls.get({
-    owner: repositoryOwner,
-    repo: "aria-practices",
-    pull_number: process.env.APG_PR_NUMBER,
-  });
-  console.info("pulls.get.success");
+  let getApgPrResult;
+
+  try {
+    getApgPrResult = await octokit.rest.pulls.get({
+      owner: repositoryOwner,
+      repo: 'aria-practices',
+      pull_number: process.env.APG_PR_NUMBER,
+    });
+    console.info('pulls.get.success');
+  } catch (e) {
+    console.error('error.get.pull.request', e);
+    process.exit(ERROR_GET_PULL_REQUEST);
+  }
 
   // Needed to append error to PR body
-  let apgPrBody = getApgPrResult.data.body || "";
+  let apgPrBody = getApgPrResult.data.body || '';
 
   // Find index of existing successful or failure build links in the body
-  let previewLinkIndex = apgPrBody.indexOf("[WAI Preview Link]");
+  let previewLinkIndex = apgPrBody.indexOf('[WAI Preview Link]');
   if (previewLinkIndex < 0) {
     // Need to account for error content
     // Assumes a previous link didn't exist, find error content
-    previewLinkIndex = apgPrBody.indexOf("WAI Preview Link [failed to build]");
+    previewLinkIndex = apgPrBody.indexOf('WAI Preview Link [failed to build]');
   }
 
-  const previewLinkUrl = isSuccess
+  const previewLinkUrl = isSiteFilesUpdateSuccess
     ? `https://deploy-preview-${
         waiPrNumber || createPullRequestResult.data.number
       }--${previewLink}/ARIA/apg`
     : `https://github.com/${repositoryOwner}/wai-aria-practices/runs/${jobId}?check_suite_focus=true`;
-  const additionalBodyContent = isSuccess
+  const additionalBodyContent = isSiteFilesUpdateSuccess
     ? `[WAI Preview Link](${previewLinkUrl}) _(Last built on ${new Date().toUTCString()})._`
     : `WAI Preview Link [failed to build](${previewLinkUrl}) on 'Update site files' step. _(Last tried on ${new Date().toUTCString()})._`;
   if (previewLinkIndex < 0) {
@@ -47,24 +65,54 @@ const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
     apgPrBody = apgPrBody.replace(stringRemainder, additionalBodyContent);
   }
 
-  await octokit.rest.pulls.update({
-    owner: repositoryOwner,
-    repo: "aria-practices",
-    pull_number: process.env.APG_PR_NUMBER,
-    body: apgPrBody,
-  });
-  console.info("pulls.update.success", apgPrBody);
+  try {
+    await octokit.rest.pulls.update({
+      owner: repositoryOwner,
+      repo: 'aria-practices',
+      pull_number: process.env.APG_PR_NUMBER,
+      body: apgPrBody,
+    });
+    console.info('pulls.update.success', apgPrBody);
+  } catch (e) {
+    console.error('error.update.pull.request', e);
+    process.exit(ERROR_UPDATE_PULL_REQUEST);
+  }
+
+  // Means there was still an error updating the WAI Preview Link
+  if (!isSiteFilesUpdateSuccess) {
+    console.error('error.site.files.update', additionalBodyContent);
+
+    // TODO: Uncomment when updated bot token is being used
+    /*try {
+      // Display build error on triggering PR's commit
+      await octokit.rest.repos.createCommitStatus({
+        owner: repositoryOwner,
+        repo: 'aria-practices',
+        sha: process.env.APG_SHA,
+        state: 'failure',
+        target_url: previewLinkUrl, // Populated by the failing CI job:step link
+        description: `WAI Preview Link failed to build in wai-aria-practices/pr-create (${ERROR_SITE_FILES_UPDATE})`,
+        context: 'WAI Preview Link failed to build',
+      });
+    } catch (e) {
+      console.error('error.create.commit.status', e);
+      process.exit(ERROR_CREATE_COMMIT_STATUS);
+    }*/
+
+    process.exit(ERROR_SITE_FILES_UPDATE);
+  }
 };
 
 (async () => {
   let waiPrNumber;
   let createPullRequestResult;
 
-  if (!isSuccess) {
+  if (!isSiteFilesUpdateSuccess) {
     try {
       await updateApgPrBody();
     } catch (e) {
-      console.error("failure.1", e);
+      console.error('error.apg.body.update', e);
+      process.exit(ERROR_APG_BODY_UPDATE);
     }
     return;
   }
@@ -73,36 +121,47 @@ const updateApgPrBody = async (waiPrNumber, createPullRequestResult) => {
     // Get current list of PRs for this repository
     const pullsListResult = await octokit.rest.pulls.list({
       owner: repositoryOwner,
-      repo: "wai-aria-practices",
+      repo: 'wai-aria-practices',
     });
-    console.info("pulls.list.success");
+    console.info('pulls.list.success');
 
     // Check to see if a PR already exists with the head branch and use that PR's number instead
     for (let i = 0; i < pullsListResult.data.length; i++) {
       let data = pullsListResult.data[i];
-      if (data.head.ref === "apg/" + process.env.APG_BRANCH) {
+      if (data.head.ref === GENERATED_APG_WAI_BRANCH) {
         waiPrNumber = data.number;
         break;
       }
     }
+  } catch (e) {
+    console.error('error.list.pull.requests', e);
+    process.exit(ERROR_LIST_PULL_REQUESTS);
+  }
 
-    if (!waiPrNumber) {
+  if (!waiPrNumber) {
+    try {
       // Create a PR if none exists
       createPullRequestResult = await octokit.rest.pulls.create({
         owner: repositoryOwner,
-        repo: "wai-aria-practices",
-        head: "apg/" + process.env.APG_BRANCH,
-        base: "main",
-        title: "apg/" + process.env.APG_BRANCH + " generated by aria-practices",
+        repo: 'wai-aria-practices',
+        head: GENERATED_APG_WAI_BRANCH,
+        base: 'main',
+        title: GENERATED_APG_WAI_BRANCH + ' generated by aria-practices',
         body: `Generated by ${repositoryOwner}/aria-practices#${process.env.APG_PR_NUMBER}.`,
         draft: true,
         maintainer_can_modify: true,
       });
-      console.info("pulls.create.success");
+      console.info('pulls.create.success');
+    } catch (e) {
+      console.error('error.create.pull.request', e);
+      process.exit(ERROR_CREATE_PULL_REQUEST);
     }
+  }
 
+  try {
     await updateApgPrBody(waiPrNumber, createPullRequestResult);
   } catch (e) {
-    console.error("failure.block.2", e);
+    console.error('error.apg.body.update', e);
+    process.exit(ERROR_APG_BODY_UPDATE);
   }
 })();
