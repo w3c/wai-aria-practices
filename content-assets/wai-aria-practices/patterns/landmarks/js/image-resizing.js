94270c1da9f2ea08584ea77b29f1672ad681da81
w3cgruntbot <w3cgruntbot@users.noreply.github.com>
2025-01-29 01:49:28 +0000

    chore: Update branch with latest `landmark-example` changes and submodule updates

--- content-assets/wai-aria-practices/patterns/landmarks/js/image-resizing.js
+++ content-assets/wai-aria-practices/patterns/landmarks/js/image-resizing.js
@@ -1,7 +1,17 @@
-/* image-resizing.js */
-function resizeImagesInFigures () {
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:  image-resizing.js
+ *
+ *   Desc:   Resizes images to fit in figure elements
+ */
+
+'use strict';
+
+function resizeImagesInFigures() {
   const figures = document.querySelectorAll('#at figure');
-  for(let i = 0; i < figures.length; i += 1) {
+  for(let i = 0; i < figures.length; i++) {
     const image = figures[i].querySelector('img');
     if (image) {
       const rect = figures[i].getBoundingClientRect();
