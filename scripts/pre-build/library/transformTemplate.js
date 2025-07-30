56686f8a259a757e64a4bef43b74e9d4dacfdd1c
Howard Edwards <howarde.edwards@gmail.com>
2025-07-30 13:16:22 -0400

    Update transformTemplate.js

--- scripts/pre-build/library/transformTemplate.js
+++ scripts/pre-build/library/transformTemplate.js
@@ -1,18 +1,4 @@
-const { parse: parseHtml } = require("node-html-parser");
-
 const transformTemplate = async (sourcePath, sourceContents) => {
-  if (sourcePath.endsWith("content/shared/templates/read-this-first.html")) {
-    // const html = parseHtml(sourceContents);
-    // const imgTags = html.querySelectorAll("img[src]");
-    // imgTags.forEach((img) => {
-    //   const src = img.getAttribute("src");
-    //   if (src.includes("/images/read-this-first.svg")) {
-    //     img.setAttribute("src", "../../content-images/wai-aria-practices/images/read-this-first.svg");
-    //   }
-    // });
-    // return html.outerHTML;
-  }
-
   return sourceContents;
 };
 
